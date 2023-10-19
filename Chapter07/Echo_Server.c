25e0d9796dc0a5fdd3ef469c7c021fdf9856edba
Ju Jae Myeong <74813599+haru1367@users.noreply.github.com>
2023-10-19 15:09:00 +0900

    Create Echo_Server.c

---
+++ Chapter07/Echo_Server.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <pthread.h>
+
+#define BUF_SIZE 1024
+#define NUM_THREAD 5
+
+void error_handling(char *message);
+void* thread_main(void* arg);
+
+int main(int argc, char *argv[])
+{
+	int serv_sock, clnt_sock;
+	char message[BUF_SIZE];
+	int str_len, i;
+	pthread_t thread_id[NUM_THREAD];
+
+	struct sockaddr_in serv_adr;
+	struct sockaddr_in clnt_adr;
+	socklen_t clnt_adr_sz;
+	
+	if(argc!=2) {
+		printf("Usage : %s <port>\n", argv[0]);
+		exit(1);
+	}
+	
+	serv_sock=socket(PF_INET, SOCK_STREAM, 0);   
+	if(serv_sock==-1)
+		error_handling("socket() error");
+	
+	memset(&serv_adr, 0, sizeof(serv_adr));
+	serv_adr.sin_family=AF_INET;
+	serv_adr.sin_addr.s_addr=htonl(INADDR_ANY);
+	serv_adr.sin_port=htons(atoi(argv[1]));
+
+	if(bind(serv_sock, (struct sockaddr*)&serv_adr, sizeof(serv_adr))==-1)
+		error_handling("bind() error");
+	
+	if(listen(serv_sock, 5)==-1)
+		error_handling("listen() error");
+	
+	clnt_adr_sz=sizeof(clnt_adr);
+
+	for(i=0; i<5; i++)
+	{
+		clnt_sock=accept(serv_sock, (struct sockaddr*)&clnt_adr, &clnt_adr_sz);
+		if(clnt_sock==-1)
+			error_handling("accept() error");
+		else
+			printf("Connected client %d \n", i+1);
+
+        // 새로운 쓰레드를 생성하고 클라이언트 소켓을 인자로 전달
+        pthread_create(&thread_id[i], NULL, thread_main, (void*)&clnt_sock);
+        // 쓰레드를 백그라운드에서 실행 (non-blocking)
+        pthread_detach(thread_id[i]);
+	}
+
+	close(serv_sock);
+	return 0;
+}
+
+void error_handling(char *message)
+{
+	fputs(message, stderr);
+	fputc('\n', stderr);
+	exit(1);
+}
+
+void* thread_main(void* arg) {
+    int str_len;
+    int clnt = *((int*)arg);
+    char message[BUF_SIZE];
+    while((str_len=read(clnt, message, BUF_SIZE))!=0)
+			write(clnt, message, str_len);
+    return NULL;
+}
